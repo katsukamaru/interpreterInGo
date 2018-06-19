f18cd8bd0d2a8c8f7009ca90c7fbef134c748635
shin.katsumaru <shin.katsumaru@supership.jp>
2018-06-19 16:43:04 +0900

    add other tokens

--- token/token.go
+++ token/token.go
@@ -28,6 +28,13 @@ const (
 
 	ASSIGN    = "ASSIGN"
 	PLUS      = "+"
+	MINUS      = "-"
+	BANG      = "!"
+	ASTERISK      = "*"
+	SLASH      = "/"
+	LT      = "<"
+	GT      = ">"
+
 	COMMA     = ","
 	SEMICOLON = ";"
 
